c8e7c8106ab1ef7b7dce30e318d498ba5eebb761
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2020-11-24 05:19:20 -0500

    fix verify endpoint

--- verifyTwitter.js
+++ verifyTwitter.js
@@ -1,25 +1,26 @@
 import handler from "./libs/handler-lib.js";
 import axios from 'axios';
 
-export const main = handler(async (event, context) => {
+export const main = handler((event, context) => {
   const userId = event.pathParameters.id;
   const twitterAppKey = process.env.twitterAppKey;
-  let verified = false;
+  let verified = { "verified": false };
 
   axios.defaults.headers.common['Authorization'] = `Bearer ${twitterAppKey}`;
 
-  axios({ method: 'get', url: 'https://api.twitter.com/1.1/members.json?list_id=1327835085010313219' })
-  .then(res2 => { 
+  return axios({ method: 'get', url: 'https://api.twitter.com/1.1/lists/members.json?list_id=1327835085010313219' })
+  .then(res => {
     Promise.all(
-      res2.data.users.map(user => {
-        if (userId === id) {
-          verified = true;
+      res.data.users.map(user => {
+        if (user.screen_name === userId) {
+          verified = { "verified": true };
         }
       })
     ).then(() => {
       return verified;
     });
   })
+  .then(() => { return verified; })
   .catch(err => {
     return err;
   });
