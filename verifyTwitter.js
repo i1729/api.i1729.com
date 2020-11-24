31d417f9b6ed632c1c0efb648b957a8a0145aef7
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2020-11-24 04:23:45 -0500

    add verifyTwitter endpoint

---
+++ verifyTwitter.js
@@ -0,0 +1,27 @@
+import handler from "./libs/handler-lib.js";
+import axios from 'axios';
+
+export const main = handler(async (event, context) => {
+  const userId = event.pathParameters.id;
+  const twitterApiKey = process.env.twitterApiKey;
+  const twitterConsumerKey = process.env.twitterConsumerKey;
+  const twitterSecretKey = process.env.twitterSecretKey;
+  let verified = false;
+  
+  axios.defaults.headers.common['Authorization'] = `Bearer ${twitterApiKey}`;
+  
+  axios({ method: 'get', url: 'https://api.twitter.com/1.1/members.json?list_id=1327835085010313219' })
+  .then(res2 => { 
+    let result = await Promise.all(
+      res2.data.users.map(user => {
+        if (user.id === id) { 
+          verified = true;
+        }  
+      });
+    );
+    return verified;
+  })
+  .catch(err => {
+    return err; 
+  });
+}
