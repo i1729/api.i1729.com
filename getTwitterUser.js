ded685bd094224ce276623405abe14ac45b794a2
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2020-11-25 20:18:37 -0600

    fix

--- getTwitterUser.js
+++ getTwitterUser.js
@@ -7,23 +7,26 @@ export const main = handler(async (event, context) => {
 
   axios.defaults.headers.common['Authorization'] = `Bearer ${twitterApiKey}`;
 
-  let user = {};
+  let user = {
+    contributor1729: false
+  };
 
   const data = await axios({ method: 'get', url: 'https://api.twitter.com/1.1/users/lookup.json?user_id=' + userId });
   const userData= data.data[0];
-  user = {
+  Object.assign(user, {
     screen_name: userData.screen_name,
     verified: userData.verified,
-    followers_count: userData.followers_count,
-    contributor1729: false,
-  };
+    followers_count: userData.followers_count
+  });
 
   const memberList = await axios({ method: 'get', url: 'https://api.twitter.com/1.1/lists/members.json?count=5000&list_id=1327835085010313219' });
 
   return Promise.all(
     memberList.data.users.map(apiUser => {
-      if (apiUser.id === user.id) {
-        Object.assign(user, {contributor1729: true});
+      if (apiUser.id_str == userData.id_str) {
+        Object.assign(user, {
+          contributor1729: true
+        });
       }
     })
   ).then(() => {
