eb3d702c02a91b841f5803dfd4115a9f47ba1170
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2020-11-29 23:53:58 -0600

    private twitter list working

--- getTwitterUser.js
+++ getTwitterUser.js
@@ -1,7 +1,6 @@
 import handler from "./libs/handler-lib.js";
 import axios from "axios";
-import { v1 as uuidv1 } from "uuid";
-import oauthSignature from 'oauth-signature';
+import oauthSignature from "oauth-signature";
 
 export const main = handler(async (event, context) => {
   const userId = event.pathParameters.id;
@@ -11,6 +10,15 @@ export const main = handler(async (event, context) => {
   const twitterOauthConsumerSecret = process.env.twitterOauthConsumerSecret;
   const twitterOauthAccessToken = process.env.twitterOauthAccessToken;
   const twitterOauthAccessTokenSecret = process.env.twitterOauthAccessTokenSecret;
+
+  const nonceGenerator = (length) => {
+    var nonce = "";
+    var allowed = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for(var i = 0; i < length; i++) {
+      nonce = nonce.concat(allowed.charAt(Math.floor(Math.random() * allowed.length)));
+    }
+    return nonce;
+  };
 
   let user = {
     contributor1729: false
@@ -31,30 +39,40 @@ export const main = handler(async (event, context) => {
       followers_count: userData.followers_count
     });
   } catch(err) {
-    console.log(err);
+    console.log("lookup" + err);
+    return "lookup " + err;
   }
 
-  const baseUrl = "https://api.twitter.com/1.1/lists/members/show.json";
-  const timestamp = Math.floor(Date.now() / 1000);
-  const nonce = uuidv1();
-  const parameters = {
-    list_id: 1333184036701925388,
-    user_id: userId,
-    oauth_consumer_key: twitterOauthConsumerKey,
-    oauth_token: twitterOauthAccessToken,
-    oauth_signature_method: 'HMAC-SHA1',
-    oauth_timestamp: timestamp,
-    oauth_nonce: nonce,
-    oauth_version: '1.0'
-  };
-  const signature = oauthSignature.generate("GET", baseUrl, parameters, twitterOauthConsumerSecret, twitterOauthAccessTokenSecret);
-  const options = {
-    headers: {
-      "Authorization": `OAuth oauth_consumer_key="${twitterOauthConsumerKey}",oauth_token="${twitterOauthAccessToken}",oauth_signature_method="HMAC-SHA1",oauth_timestamp="${timestamp}",oauth_nonce="${nonce}",oauth_version="1.0",oauth_signature="${signature}"`
-    }
-  };
-
   try {
+    const baseUrl = "https://api.twitter.com/1.1/lists/members/show.json";
+    const timestamp = Math.floor(Date.now() / 1000);
+    const nonce = nonceGenerator(11);
+    const parameters = {
+      list_id: '1333184036701925388',
+      user_id: userId,
+      oauth_consumer_key: twitterOauthConsumerKey,
+      oauth_token: twitterOauthAccessToken,
+      oauth_signature_method: 'HMAC-SHA1',
+      oauth_timestamp: timestamp.toString(),
+      oauth_nonce: nonce,
+      oauth_version: '1.0'
+    };
+
+    console.log("parameters: ", parameters);
+
+    var signatureOptions = {
+      encodeSignature: true // will encode the signature following the RFC 3986 Spec by default
+    };
+
+    const encoded_oauth_signature = oauthSignature.generate('GET', baseUrl, parameters, twitterOauthConsumerSecret, twitterOauthAccessTokenSecret, signatureOptions);
+
+
+    const options = {
+      headers: {
+        "Authorization": `OAuth oauth_consumer_key="${twitterOauthConsumerKey}",oauth_token="${twitterOauthAccessToken}",oauth_signature_method="HMAC-SHA1",oauth_timestamp="${timestamp}",oauth_nonce="${nonce}",oauth_version="1.0",oauth_signature="${encoded_oauth_signature}"`
+      }
+    };
+
     const url = "https://api.twitter.com/1.1/lists/members/show.json?list_id=1333184036701925388&user_id=" + userId;
     const memberList = await axios.get(url, options);
 
@@ -67,7 +85,7 @@ export const main = handler(async (event, context) => {
       return user;
     }
   } catch (err) {
-    console.log(err.response.data.errors);
+    console.log(err);
     return err.response.data.errors;
   }
 });
