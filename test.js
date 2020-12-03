96857bb3d80f1d6a737ea9087811f7d8715b1eb5
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2020-12-02 19:15:38 -0600

    it works

--- test.js
+++ test.js
@@ -30,7 +30,7 @@ const test = async () => {
   const rsv = ethUtil.fromRpcSig(signedData);
 
   // MembershipClaimsRegistry contract address and ABI
-  const contractAddress = "0xeeac4AE471f1132cabE21C10404A09Bf23BDE0c9";
+  const contractAddress = "0xeE64ad43Ec053b7Ce4bd8F0896ffc26e7e8Bc171";
   const contractAbi = [ { "inputs": [ { "internalType": "address", "name": "_verifier", "type": "address" } ], "stateMutability": "nonpayable", "type": "constructor" }, { "inputs": [ { "components": [ { "internalType": "address", "name": "issuer", "type": "address" }, { "internalType": "address", "name": "subject", "type": "address" }, { "internalType": "uint256", "name": "validFrom", "type": "uint256" }, { "internalType": "uint256", "name": "validTo", "type": "uint256" } ], "internalType": "struct Claim", "name": "claim", "type": "tuple" }, { "internalType": "uint8", "name": "v", "type": "uint8" }, { "internalType": "bytes32", "name": "r", "type": "bytes32" }, { "internalType": "bytes32", "name": "s", "type": "bytes32" } ], "name": "addClaim", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "components": [ { "internalType": "address", "name": "issuer", "type": "address" }, { "internalType": "address", "name": "subject", "type": "address" }, { "internalType": "uint256", "name": "validFrom", "type": "uint256" }, { "internalType": "uint256", "name": "validTo", "type": "uint256" } ], "internalType": "struct Claim", "name": "claim", "type": "tuple" } ], "name": "checkClaim", "outputs": [ { "internalType": "bool", "name": "", "type": "bool" } ], "stateMutability": "view", "type": "function" } ];
 
   // Initialize and try to call contract
@@ -38,7 +38,11 @@ const test = async () => {
   const gasPrice = await web3.eth.getGasPrice();
   try {
     const gas = await contract.methods.addClaim(message, rsv.v, rsv.r, rsv.s).estimateGas({ from: web3.eth.defaultAccount });
-    contract.methods.addClaim(message, rsv.v, rsv.r, rsv.s).send({ from: web3.eth.defaultAccount, gasPrice, gas: "32320" }).then(console.log);
+    contract.methods.addClaim(message, rsv.v, rsv.r, rsv.s).send({ from: web3.eth.defaultAccount, gasPrice, gas }).then(async (res) => {
+      console.log(res);
+      const gasCheck = await contract.methods.checkClaim(message).estimateGas({ from: web3.eth.defaultAccount });
+      contract.methods.checkClaim(message).call({ from: web3.eth.defaultAccount, gasPrice, gas }).then(console.log); 
+    });
   } catch (err) {
     console.log("ERRORED!!!!!!!!!!!!!!!!!!!");
     console.log(err);
